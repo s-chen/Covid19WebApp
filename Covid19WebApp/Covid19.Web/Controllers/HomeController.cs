09ad83371e98d5b3927cf6b2007a7d1ace2ecdc8
Si Chen <s-chen@hotmail.co.uk>
2020-06-21 17:15:23 +0100

    Country list, api integration

--- Covid19WebApp/Covid19.Web/Controllers/HomeController.cs
+++ Covid19WebApp/Covid19.Web/Controllers/HomeController.cs
@@ -1,21 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Covid19.Web.Models;
-
-namespace Covid19.Web.Controllers
+﻿namespace Covid19.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Covid19.Core.RetrieveDataService;
+    using Covid19.Web.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRetrieveDataService retrieveDataService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRetrieveDataService retrieveDataService)
         {
             _logger = logger;
+            this.retrieveDataService = retrieveDataService;
         }
 
         public IActionResult Index()
@@ -26,6 +29,12 @@ namespace Covid19.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<ViewResult> Countries()
+        {
+            var countries = await this.retrieveDataService.GetCountriesAsync();
+            return this.View(countries);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Covid19WebApp/Covid19.Web/Startup.cs
+++ Covid19WebApp/Covid19.Web/Startup.cs
@@ -1,16 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 namespace Covid19.Web
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Covid19.Core.RetrieveDataService;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -24,6 +25,7 @@ namespace Covid19.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IRetrieveDataService, RetrieveDataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
